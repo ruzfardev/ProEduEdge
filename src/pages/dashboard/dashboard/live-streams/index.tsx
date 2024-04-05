1fa590c53c228d0fc19c812c661cbde11941e15c
Farrukh Ruzmetov <farrukhruzmetov2002@gmail.com>
2024-04-05 14:44:56 +0500

    favicon configured

--- src/pages/dashboard/dashboard/live-streams/index.tsx
+++ src/pages/dashboard/dashboard/live-streams/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {ListOfRecordings} from '@/pages/dashboard/dashboard/live-streams/list-recordings';
-
 export const MeetingRoot = () => {
 	return (
 		<div className="h-full">
